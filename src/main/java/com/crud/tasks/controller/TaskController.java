31dd45f6231b7486fbc08d34bfd820ae9a883f37
akecka <agnkecka@gmail.com>
2017-12-14 18:37:10 +0100

    Mapping

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -10,26 +10,26 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    @RequestMapping(method = RequestMethod.GET, value = "/getTasks")
     public List<TaskDto> getTasks() {
         return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTask", params = "id")
+    @RequestMapping(method = RequestMethod.GET, value = "/getTask", params = "id")
     public TaskDto getTask(@RequestParam("id") long id) {
         return new TaskDto(id, "test title", "test_content");
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
+    @RequestMapping(method = RequestMethod.DELETE, value = "/deleteTask")
     public void deleteTask(String taskId) {
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "updateTask", params = {"id"})
+    @RequestMapping(method = RequestMethod.PUT, value = "/updateTask", params = {"id"})
     public TaskDto updateTask(@RequestParam("id") long id) {
         return new TaskDto(id, "Edited Title", "Edited Content");
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createTask")
+    @RequestMapping(method = RequestMethod.POST, value = "/createTask")
     public void createTask(TaskDto taskDto) {
     }
     /*@RequestMapping(value = "", method = RequestMethod.GET)
