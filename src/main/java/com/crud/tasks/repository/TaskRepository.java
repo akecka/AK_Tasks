f03c794f7cb91076d279b60ecd79804942797b7d
akecka <agnkecka@gmail.com>
2017-12-17 12:13:58 +0100

    Spring

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,14 +1,13 @@
-package com.crud.task.repository;
+package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
-//Should it be @Repository ??
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
+
+
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,6 +1,6 @@
 package com.crud.tasks.service;
 
-import com.crud.task.repository.TaskRepository;
+import com.crud.tasks.repository.TaskRepository;
 import com.crud.tasks.domain.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +12,13 @@ public class DbService {
     @Autowired
     private TaskRepository repository;
 
-    public List<Task> getAllTasks(){
+    public List<Task> getAllTasks() {
         return repository.findAll();
     }
+
+    public Task getTaskById(final Long id) {
+        return repository.findById(id).orElse(null);
+
+    }
+
 }
