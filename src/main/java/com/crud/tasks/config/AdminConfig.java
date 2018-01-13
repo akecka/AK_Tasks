bd9a0cf5733ce1484a8d7786d44c2ed6a2bf0e05
akecka <agnkecka@gmail.com>
2018-01-13 17:42:24 +0100

    mail

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -2,7 +2,7 @@ package com.crud.tasks.config;
 
 
 import lombok.Getter;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
