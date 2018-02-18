2c6289fab64e47bc54174f12ecee11da7061b8e8
akecka <agnkecka@gmail.com>
2018-02-18 12:20:41 +0100

    mail

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 
+import com.crud.tasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,9 @@ import org.thymeleaf.context.Context;
 public class MailCreatorService {
 
     @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
@@ -20,8 +24,10 @@ public class MailCreatorService {
         context.setVariable("message", message);
         context.setVariable("tasks_url", "https://akecka.github.io/");
         context.setVariable("button", "Visit website");
-        return templateEngine.process("mail/created-trello-card-mail", context);
-
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        return templateEngine.process("src/main/resources/templates.mail/created-trello-card-mail.html", context);
+        //templates.mail/created-trello-card-mail.html
+        //C:\Users\Borsuk\Downloads\tasks\tasks\src\main\resources\templates.mail\created-trello-card-mail.html
     }
 }
 
